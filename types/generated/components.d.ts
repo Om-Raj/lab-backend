496ad36056e3993be13bdedf87ed1f2db46cc71c
AYUSHSAHU2004 <141218241+AYUSHSAHU2004@users.noreply.github.com>
2024-08-08 00:01:23 +0530

    added repeatable component in research section

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,16 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SmallParagraphTheme extends Schema.Component {
+  collectionName: 'components_small_paragraph_themes';
+  info: {
+    displayName: 'Theme';
+    description: '';
+  };
+  attributes: {
+    Theme: Attribute.String;
+  };
+}
+
 export interface SmallParagraphPapersPublished extends Schema.Component {
   collectionName: 'components_small_paragraph_papers_publisheds';
   info: {
@@ -33,6 +44,18 @@ export interface ResearchConclusionParaReasearchSummary
   };
 }
 
+export interface ReasearchContentResearchContent extends Schema.Component {
+  collectionName: 'components_reasearch_content_research_contents';
+  info: {
+    displayName: 'ResearchContent';
+  };
+  attributes: {
+    Image: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    ReasearchContent: Attribute.Text;
+    Link: Attribute.String;
+  };
+}
+
 export interface ParagraphContentParagraphContent extends Schema.Component {
   collectionName: 'components_paragraph_content_paragraph_contents';
   info: {
@@ -52,9 +75,11 @@ export interface ParagraphContentParagraphContent extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'small-paragraph.theme': SmallParagraphTheme;
       'small-paragraph.papers-published': SmallParagraphPapersPublished;
       'small-paragraph.members': SmallParagraphMembers;
       'research-conclusion-para.reasearch-summary': ResearchConclusionParaReasearchSummary;
+      'reasearch-content.research-content': ReasearchContentResearchContent;
       'paragraph-content.paragraph-content': ParagraphContentParagraphContent;
     }
   }
